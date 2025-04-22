185100a99a8e5e49cff027508d3f772ca76f5fc4
Bruno Remeikis <bremeikis@softexpert.com>
2025-04-22 10:27:57 -0300

    Retorna sugestões da IA em formato JSON List / Remove código não utilizado

--- app/ai_service.py
+++ app/ai_service.py
@@ -3,6 +3,8 @@ from ai_connectors.googleai_connector import GoogleAiCoonnector
 
 from utils.string_utils import StringBuilder
 
+import json
+
 
 ai: AiConnector = GoogleAiCoonnector()
 
@@ -10,10 +12,16 @@ ai: AiConnector = GoogleAiCoonnector()
 def ask(doc_content):
     sb = StringBuilder()
     sb.ln('O documento abaixo é uma produção acadêmico-científica.')
-    sb.ln('Traga artigos científicos que tenham relação com o conteúdo da produção.')
     sb.ln('```html')
     sb.ln(doc_content)
     sb.ln('```')
+    sb.ln('Traga artigos científicos que tenham relação com o conteúdo da produção, a fim de enriquecê-la.')
+    sb.ln('Sua resposta deve ser uma lista JSON; Cada referência deve ser trazida no formato JSON com a seguinte configuração:')
+    sb.ln('- Deve conter o campo "title", contendo o tídulo da referência')
+    sb.ln('- Deve conter o campo "link", contendo o link da referência')
+    sb.ln('- Deve conter o campo "suggestion", contendo uma sugestão de citação da referência')
+    sb.ln('Sua resposta deve ser apenas o JSON plano. Não utilize quebras de linha ou "Fenced Code Blocks".')
 
     res = ai.ask(sb.toString())
-    return res
+    json_res = json.loads(res)
+    return json_res

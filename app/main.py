233f41bed91410e3a5a9cfcfe146844568bf36e8
caslu <sepulchrolucas@gmail.com>
2025-11-03 12:38:36 -0300

    Add CORS middleware to allow specific origins

--- app/main.py
+++ app/main.py
@@ -2,10 +2,23 @@ from fastapi import FastAPI
 
 from app.config import routes
 
+from fastapi.middleware.cors import CORSMiddleware
+
+ALLOWED_ORIGINS = [
+    "http://localhost",
+    "https://paperhub.com.br"
+]
 
 def create_app():
     app = FastAPI()
     app.include_router(routes.ai_router)
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=ALLOWED_ORIGINS,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
     return app
 
 application = create_app()
